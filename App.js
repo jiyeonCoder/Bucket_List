605a80e1aabd9adce96f69042c0fcfe61bdaf7a2
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-09-28 20:19:40 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import logo from "./logo.svg";
-import "./style.css";
-
+//import logo from "./logo.svg";
+//import "./App.css";
+//import "./style.css";
+import styled from "styled-components";
 import React from "react";
 import BucketList from "./BucketList";
 
@@ -11,54 +12,91 @@ class App extends React.Component {
     this.state = {
       list: ["Apple", "Kiwis", "Water Melon"],
     };
+
+    this.text = React.createRef();
+
+    console.log(this.text);
   }
 
+  componentDidMount() {
+    console.log(this.text);
+  }
+
+  addBucket = () => {
+    console.log(this.text.current.value);
+    const newItem = this.text.current.value;
+    this.setState({ list: [...this.state.list, newItem] });
+  };
+
   render() {
+    //console.log(this.state.list);
+    //console.log(this.text.current);
     return (
-      <div className="App">
-        <div className="container">
-          <h1>Things To Buy</h1>
-          <hr className="line" />
+      <MyForm>
+        <Container>
+          <Title>Things To Buy</Title>
+          <Line />
           <BucketList list={this.state.list} />
-        </div>
-      </div>
+        </Container>
+
+        <InputWrap>
+          <input
+            type="text"
+            ref={this.text}
+            onChange={() => {
+              console.log(this.text.current.value);
+            }}
+          />
+          <button onClick={this.addBucket}>Add</button>
+        </InputWrap>
+      </MyForm>
     );
   }
 }
 
+// const MyStyled = styled.div`
+//   width: 50vw;
+//   height: 150px;
+//   background-color: ${(props) => (props.bg_color ? "red" : "purple")};
+//   p {
+//     color: blue;
+//   }
+//   &:hover {
+//     background-color: yellow;
+//   }
+// `;
+
 const MyForm = styled.div`
-  .App {
-    background-color: #eee;
-    height: 100vh;
-    width: 100vw;
-    display: flex;
-  }
-
-  .container {
-    background-color: #fff;
-    width: 50vw;
-    max-width: 350px;
-    margin: auto;
-    height: 80vh;
-    padding: 16px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-  }
-  .container > h1 {
-    color: slateblue;
-    text-align: center;
-  }
-
-  .container > .line {
-    margin: 16px 0px;
-  }
-
-  .list-item {
-    padding: 16px;
-    margin: 8px;
-    background-color: aliceblue;
-  }
+  background-color: #eee;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
 `;
 
+const InputWrap = styled.div`
+  padding: 16px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
+const Container = styled.div`
+  background-color: #fff;
+  width: 50vw;
+  max-width: 350px;
+  margin: auto;
+  height: 80vh;
+  padding: 16px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
+
+const Title = styled.h1`
+  color: slateblue;
+  text-align: center;
+`;
+
+const Line = styled.hr`
+  margin: 16px 0px;
+`;
 
 export default App;
